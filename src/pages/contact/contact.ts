f02afc48f3c6dfa14ac919bdfaf7b3ab73d77848
Heum Sinat <heumsinatgic25@gmail.com>
2017-07-01 17:47:06 +0700

    add project folder

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,8 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  contacts: any;
   constructor(public navCtrl: NavController) {
+    this.contacts = [
+            'Bread',
+            'Milk',
+            'Cheese',
+            'Snacks',
+            'Apples',
+            'Bananas',
+            'Peanut Butter',
+            'Chocolate',
+            'Avocada',
+            'Vegemite',
+            'Muffins',
+            'Paper towels'
+        ];
 
   }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public  nav: NavController) {
 
   }
+
+  page1() {
+      this.nav.push(ContactPage);
+    }
+  page2() {
+        this.nav.push(AboutPage);
+      }
+
 
 }
